31f4acb5bd41767d9dbfe0f723ba2b8335f1404a
tjdudwns <tjdudwns@gmail.com>
2020-04-08 08:30:31 +0900

    second

--- src/main/java/com/example/demo/Order.java
+++ src/main/java/com/example/demo/Order.java
@@ -1,8 +1,8 @@
 package com.example.demo;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cloud.stream.messaging.Processor;
-
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.support.MessageBuilder;
@@ -10,9 +10,8 @@ import org.springframework.util.MimeTypeUtils;
 
 import javax.persistence.*;
 
-
 @Entity
-@Table(name = "ORDER_TABLE")
+@Table(name="ORDER_TABLE")
 public class Order {
     @Id @GeneratedValue
     Long id;
@@ -20,6 +19,31 @@ public class Order {
     Long productId;
     String productName;
 
+    @PostPersist
+    public void eventPublish(){
+        OrderPlaced orderPlaced = new OrderPlaced();
+        orderPlaced.setOrderId(this.getId());
+        orderPlaced.setProductId(this.getProductId());
+        orderPlaced.setProductName(this.getProductName());
+        orderPlaced.setQty(this.getQty());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+
+        try {
+            json = objectMapper.writeValueAsString(orderPlaced);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON format exception", e);
+        }
+//        System.out.println(json);
+
+        Processor processor = DemoApplication.applicationContext.getBean(Processor.class);
+        MessageChannel outputChannel = processor.output();
+
+        outputChannel.send(MessageBuilder
+                .withPayload(json)
+                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                .build());
+    }
 
     public Long getId() {
         return id;
@@ -52,28 +76,7 @@ public class Order {
     public void setProductName(String productName) {
         this.productName = productName;
     }
-
-    @PostPersist
-    public void eventPublish() {
-        OrderPlaced orderPlaced = new OrderPlaced();
-        orderPlaced.setProductId(this.getProductId());
-        orderPlaced.setProductName(this.getProductName());
-        orderPlaced.setQty(this.getQty());
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = null;
-
-        try {
-            json = objectMapper.writeValueAsString(orderPlaced);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("JSON format exception", e);
-        }
-        Processor processor = DemoApplication.applicationContext.getBean(Processor.class);
-        MessageChannel outputChannel = processor.output();
-
-        outputChannel.send(MessageBuilder
-                .withPayload(json)
-                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
-                .build());
-        //       System.out.println(json);
-    }
 }
+
+
+
--- src/main/java/com/example/demo/OrderPlaced.java
+++ src/main/java/com/example/demo/OrderPlaced.java
@@ -51,3 +51,4 @@ public class OrderPlaced {
         this.productName = productName;
     }
 }
+
